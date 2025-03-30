bd4ed6c22c85bff5a3e3abcbae08b30d55f9f7dd
Eileen Yoon <eyn@gmx.com>
2025-03-30 19:03:52 -0400

    tmp

--- queue.c
+++ queue.c
@@ -33,15 +33,17 @@ static int is_tile_done(struct game_state start)
     return found;
 }
 
-#if 0
-static int check_end(struct game_state start, int move)
+static inline int get_dx(int m)
 {
     int dx[4] = {-1,  1,  0,  0};
-    int dy[4] = { 0,  0, -1,  1};
-    int x = dx[move];
-    int y = dy[move];
+    return dx[m];
 }
-#endif
+
+static inline int get_dy(int m)
+{
+    int dy[4] = { 0,  0, -1,  1};
+    return dy[m];
+}
 
 static void print_tile(uint8_t t[4][4])
 {
@@ -56,26 +58,31 @@ static void print_tile(uint8_t t[4][4])
     printf("\n");
 }
 
-int get_dx(int m)
-{
-    int dx[4] = {-1,  1,  0,  0};
-    return dx[m];
-}
-
-int get_dy(int m)
-{
-    int dy[4] = { 0,  0, -1,  1};
-    return dy[m];
-}
+#if 0
 
 static int get_pos(struct game_state *start, struct linked_list *list)
 {
     uint8_t t[4][4];
     memcpy(t, start->tiles, sizeof(start->tiles));
-    print_tile(t);
+
     int start_row = start->empty_row;
     int start_col = start->empty_col;
 
+    for (int m = 0; m < m_count; m++)
+    {
+        int x = get_dx(m);
+        int y = get_dy(m);
+
+        int row = start_row + x;
+        int col = start_col + y;
+
+        t[start_row][start_col] = t[row][col];
+        t[row][col] = 0;
+        start_row = row;
+        start_col = col;
+    }
+
+#if 0
     // traverse ll
     struct list_node *curr = list->head;
 
@@ -100,10 +107,11 @@ static int get_pos(struct game_state *start, struct linked_list *list)
         curr = curr->next;
     }
     print_tile(t);
-
+#endif
+#if 0
     // find new paths
-    //int m_list[4] = {-1, -1, -1, -1};
-    //int m_count = 0;
+    int m_list[4] = {-1, -1, -1, -1};
+    int m_count = 0;
     for (int m = 0; m < 4; m++)
     {
         int x = get_dx(m);
@@ -116,14 +124,22 @@ static int get_pos(struct game_state *start, struct linked_list *list)
         if (value != get_expected_value(row, col))
         {
             printf("path: row: %d col: %d val: %d\n", row, col, value);
+            m_list[m_count++] = m;
         }
     }
 
+    struct game_state new;
+    memcpy(new.tiles, t, sizeof(start->tiles));
+    new.empty_row = start_row;
+    new.empty_col = start_col;
+#endif
     return 0;
 }
+#endif
 
 int number_of_moves(struct game_state start)
 {
+    print_tile(start.tiles);
     if (is_tile_done(start))
     {
         printf("found\n");
@@ -144,12 +160,14 @@ int number_of_moves(struct game_state start)
     free_list(list);
     #endif
 
-    int dx[4] = {-1,  1,  0,  0};
-    int dy[4] = { 0,  0, -1,  1};
+    //insert_at_tail(&list, serialize(start));
+
+    int m_list[4] = {-1, -1, -1, -1};
+    int m_count = 0;
     for (int m = 0; m < 4; m++)
     {
-        int x = dx[m];
-        int y = dy[m];
+        int x = get_dx(m);
+        int y = get_dy(m);
         if ((start.empty_row + x) > 3 || (start.empty_row + x) < 0) continue;
         if ((start.empty_col + y) > 3 || (start.empty_col + y) < 0) continue;
         int row = start.empty_row + x;
@@ -157,11 +175,41 @@ int number_of_moves(struct game_state start)
         if (start.tiles[row][col] != get_expected_value(row, col))
         {
             printf("path: row: %d col: %d val: %d\n", row, col, start.tiles[row][col]);
-            insert_at_tail(&list, m);
+            m_list[m_count++] = m;
         }
     }
 
-    get_pos(&start, &list);
+    for (int i = 0; i < m_count; i++)
+    {
+        int m = m_list[i];
+        struct game_state new;
+        memcpy(new.tiles, start.tiles, sizeof(start.tiles));
+        int x = get_dx(m);
+        int y = get_dy(m);
+
+        int row = start.empty_row + x;
+        int col = start.empty_col + y;
+        int value = start.tiles[row][col];
+        new.tiles[start.empty_row][start.empty_col] = value;
+        new.tiles[row][col] = 0;
+        new.empty_row = row;
+        new.empty_col = col;
+        new.num_steps = start.num_steps + 1;
+        insert_at_tail(&list, serialize(new));
+    }
+    
+    dump_list(NULL, list);
+
+    struct list_node *curr = list.head;
+    while (1)
+    {
+        if (curr == NULL) break;
+        struct game_state state = deserialize(curr->value);
+        print_tile(state.tiles);
+        curr = curr->next;
+    }
+
+    //get_pos(&start, &list);
 #if 0
     //uint8_t t[4][4];
     //memcpy(t, start.tiles, sizeof(start.tiles));
