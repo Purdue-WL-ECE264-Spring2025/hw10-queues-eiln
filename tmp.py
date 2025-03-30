980404d93f7586e6fdb2cba0f00e5319ff4a303a
Eileen Yoon <eyn@gmx.com>
2025-03-30 14:07:57 -0400

    tmp

--- tmp.py
+++ tmp.py
@@ -2,7 +2,7 @@ import numpy as np
 
 x = "1 2 3 4 5 6 0 8 9 10 7 12 13 14 11 15"
 x = "1 2 3 4 5 6 7 8 9 10 0 12 13 14 11 15"
-#x = "1 3 6 4 5 2 11 7 9 10 15 8 13 14 0 12"
+x = "1 3 6 4 5 2 11 7 9 10 15 8 13 14 0 12"
 tile = x.split()
 tile = [int(t) for t in tile]
 tile = np.array(tile)
@@ -19,26 +19,48 @@ def get_value(row, col):
     return (row * 4) + col + 1
 
 moves = 0
-for n in range(3):
-    if (tile[start_row][start_col] == 0 and start_row == 3 and start_col == 3):
-        print("Found")
-        print(f"moves: {moves}")
-        break
-    expected = get_value(start_row, start_col)
+for n in range(1):
+    #if (tile[start_row][start_col] == 0 and start_row == 3 and start_col == 3):
+    #    print("Found")
+        #print(f"moves: {moves}")
+        #break
+    #expected = get_value(start_row, start_col)
 
     stop = 0
-    for x in [-1, 0, 1]:
-        if (stop): break
+    paths = []
+    dx = [-1, 1,  0, 0]
+    dy = [ 0, 0, -1, 1]
+    for m in range(4):
+        x, y = dx[m], dy[m]
         if (start_row + x) > 3 or (start_row + x) < 0: continue
-        for y in [-1, 0, 1]:
-            if (x == 0 and y == 0): continue
-            if (start_col + y) > 3 or (start_col + y) < 0: continue
+        if (start_col + y) > 3 or (start_col + y) < 0: continue
+        row = start_row + x
+        col = start_col + y
+        value = tile[row][col]
+        if (value != get_value(row, col)): # == expected
+            print("incorrect value: ", row, col, value)
+            paths.append((x, y))
+    print(paths)
+    if (len(paths) == 1):
+        x, y = paths[0]
+        row = start_row + x
+        col = start_col + y
+        value = tile[row][col]
+        tile[start_row][start_col] = value
+        tile[row][col] = 0
+        moves += 1
+        start_row = row
+        start_col = col
+        stop = 1
+        print("moved", x, y)
+    elif len(paths) > 1:
+        print("multiple paths:")
+        for (x, y) in paths:
             row = start_row + x
             col = start_col + y
             value = tile[row][col]
-            if (value == expected):
+            if (value == get_value(row, col)): # == expected
                 print("YO", row, col, value)
-                #continue
                 tile[start_row][start_col] = value
                 tile[row][col] = 0
                 moves += 1
@@ -46,6 +68,8 @@ for n in range(3):
                 start_col = col
                 stop = 1
                 break
+        #raise ValueError("fuck")
+    print("-"*60)
 
     print(tile)
     print(start_row, start_col)
