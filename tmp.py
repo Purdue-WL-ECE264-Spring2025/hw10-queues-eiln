09003d3c4ea265b301745abeb1219110f37c810c
Eileen Yoon <eyn@gmx.com>
2025-03-30 13:42:53 -0400

    tmp

--- tmp.py
+++ tmp.py
@@ -15,30 +15,34 @@ print(start_row, start_col)
 def get_value(row, col):
     return (row * 4) + col + 1
 
-expected = get_value(start_row, start_col)
-print(expected)
 moves = 0
+for n in range(4):
+    if (tile[start_row][start_col] == 0 and start_row == 3 and start_col == 3):
+        print("Found")
+        break
+    expected = get_value(start_row, start_col)
+    print(expected)
 
-stop = 0
-for x in [-1, 0, 1]:
-    if (stop): break
-    for y in [-1, 0, 1]:
-        if (x == 0 and y == 0): continue
-        row = start_row + x
-        col = start_col + y
-        value = tile[row][col]
-        if (value == expected):
-            print("YO", row, col, value)
-            tile[start_row][start_col] = value
-            tile[row][col] = 0
-            moves += 1
-            start_row = row
-            start_col = col
-            stop = 1
-            break
+    stop = 0
+    for x in [-1, 0, 1]:
+        if (stop): break
+        for y in [-1, 0, 1]:
+            if (x == 0 and y == 0): continue
+            row = start_row + x
+            col = start_col + y
+            value = tile[row][col]
+            if (value == expected):
+                print("YO", row, col, value)
+                tile[start_row][start_col] = value
+                tile[row][col] = 0
+                moves += 1
+                start_row = row
+                start_col = col
+                stop = 1
+                break
 
-print(tile)
-print(start_row, start_col)
+    print(tile)
+    print(start_row, start_col)
 
 
 start_row = 0
